694b427d65e8f02684bdabf978f3c5037f20e742
Nmplz <dartalex2@list.ru>
2025-04-20 13:10:52 +0300

    Добавить проверки для кода про Хоббитов.

---
+++ Sprint_4_Unit_test/Hobbits_case/test_precode_2.py
@@ -0,0 +1,13 @@
+# импортируем класс HobbitsPriorityStatus из файла miles.py
+from miles import HobbitsPriorityStatus
+
+
+class TestHobbitsPriorityStatus:
+
+    def test_add_miles_and_check_status(self):
+
+        hobbits_priority = HobbitsPriorityStatus()
+        hobbits_priority.add_new_hobbit("Gollum")
+        hobbits_priority.add_miles_to_hobbit("Gollum", 50)
+        gollum_status = hobbits_priority.get_status("Gollum")
+        assert gollum_status == "Classic", "Неправильно начислен статус"
---
+++ Sprint_4_Unit_test/Hobbits_case/test_precode_3.py
@@ -0,0 +1,16 @@
+import pytest
+
+from miles import HobbitsPriorityStatus
+
+
+class TestHobbitsPriorityStatus:
+
+    # добавь параметризацию
+    @pytest.mark.parametrize("miles, status", [[299, "Classic"], [300, "Silver"]])
+    def test_add_miles_and_check_status(self, miles, status):
+        # создай нового хоббита в системе, добавь ему мили и проверь его статус
+        hobbits_priority = HobbitsPriorityStatus()
+        hobbits_priority.add_new_hobbit("Gollum")
+        hobbits_priority.add_miles_to_hobbit("Gollum", miles)
+        gollum_status = hobbits_priority.get_status("Gollum")
+        assert gollum_status == status, f"Incorrect status:expected {status}, but get {gollum_status}"
