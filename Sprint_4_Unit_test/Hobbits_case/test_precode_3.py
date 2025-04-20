f8969429cc47b2f1a9328c3fe88e99d9d27575fa
Nmplz <dartalex2@list.ru>
2025-04-20 13:40:51 +0300

    Add pytest.ini files

--- Sprint_4_Unit_test/Hobbits_case/test_precode_3.py
+++ Sprint_4_Unit_test/Hobbits_case/test_precode_3.py
@@ -4,10 +4,9 @@ from miles import HobbitsPriorityStatus
 
 class TestHobbitsPriorityStatus:
 
-    # добавь параметризацию
     @pytest.mark.parametrize("miles, status", [[299, "Classic"], [300, "Silver"]])
     def test_add_miles_and_check_status(self, miles, status):
-        # создай нового хоббита в системе, добавь ему мили и проверь его статус
+
         hobbits_priority = HobbitsPriorityStatus()
         hobbits_priority.add_new_hobbit("Gollum")
         hobbits_priority.add_miles_to_hobbit("Gollum", miles)
