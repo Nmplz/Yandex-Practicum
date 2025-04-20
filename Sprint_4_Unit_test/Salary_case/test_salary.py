6f855f62791e215e4a1f5f2ff48b37763b6c22ff
Nmplz <dartalex2@list.ru>
2025-04-20 18:28:37 +0300

    Add 100% test coverage

--- Sprint_4_Unit_test/Salary_case/test_salary.py
+++ Sprint_4_Unit_test/Salary_case/test_salary.py
@@ -5,3 +5,8 @@ class TestSalaryService:
         actual = SalaryService.calculate_salary(50000)
         expected = 2500
         assert actual == expected 
+    
+    def test_calculate_salary_when_over_limit(self):
+        actual = SalaryService.calculate_salary(1100000)
+        expected = 50000
+        assert actual == expected 
